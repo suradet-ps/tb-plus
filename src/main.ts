b7dfc9dfd6a85c9e3ffcc23896d1cdb75ed4d918
Suradet Pratomsak <pharmacistsabot@gmail.com>
2026-05-15 20:08:46 +0700

    chore: add global styles and design system variables

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './styles/variables.css';
+import './styles/base.css';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
